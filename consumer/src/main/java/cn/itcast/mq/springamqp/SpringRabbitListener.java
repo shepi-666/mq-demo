cf3d5ea574e1b03edbe286a9b95aea5e5ffb3c71
javadong <dongshaowei1201@gmail.com>
2022-01-05 16:13:15 +0800

    fanout模式下的消息队列

--- consumer/src/main/java/cn/itcast/mq/springamqp/SpringRabbitListener.java
+++ consumer/src/main/java/cn/itcast/mq/springamqp/SpringRabbitListener.java
@@ -8,15 +8,25 @@ import java.time.LocalTime;
 @Component
 public class SpringRabbitListener {
 
-    @RabbitListener(queues = "simple.queue")
+    /*@RabbitListener(queues = "simple.queue")
     public void listenWorkQueue1(String msg) throws InterruptedException {
         System.out.println("消费者1接收到的消息：【" + msg + "】" + LocalTime.now());
         Thread.sleep(20);
+    }*/
+
+    // @RabbitListener(queues = "simple.queue")
+    // public void listenWorkQueue2(String msg) throws InterruptedException {
+    //     System.err.println("消费者2接收到的消息：【" + msg + "】" + LocalTime.now());
+    //     Thread.sleep(20);
+    // }
+
+    @RabbitListener(queues = "fanout.queue1")
+    public void listenFanQueue1(String msg) throws InterruptedException {
+        System.out.println("消费者1接收到的消息：【" + msg + "】" + LocalTime.now());
     }
 
-    @RabbitListener(queues = "simple.queue")
-    public void listenWorkQueue2(String msg) throws InterruptedException {
-        System.err.println("消费者2接收到的消息：【" + msg + "】" + LocalTime.now());
-        Thread.sleep(20);
+    @RabbitListener(queues = "fanout.queue2")
+    public void listenFanQueue2(String msg) throws InterruptedException {
+        System.out.println("消费者2接收到的消息：【" + msg + "】" + LocalTime.now());
     }
 }
---
+++ publisher/src/test/java/cn/itcast/mq/spring/SpringAmqpTest.java
@@ -0,0 +1,47 @@
+package cn.itcast.mq.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import sun.awt.windows.ThemeReader;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAmqpTest {
+
+    @Resource
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    public void testSendMessage2SimpleQueue() {
+        String queueName = "simple.queue";
+        String message = "hello! spring amqp";
+        rabbitTemplate.convertAndSend(queueName, message);
+    }
+
+    @Test
+    public void testSendMessage2WorkQueue() throws InterruptedException {
+        String queueName = "simple.queue";
+        String message = "hello! spring amqp _ ";
+        for (int i = 1; i < 50; i++) {
+            rabbitTemplate.convertAndSend(queueName, message + i);
+            Thread.sleep(20);
+        }
+
+    }
+
+    @Test
+    public void testSendFanoutExchange() {
+        // 交换机名称
+        String exchangeName = "javadong.fanout";
+        // 发送的消息
+        String message = "Hello, everyone";
+
+        // 发送消息
+        rabbitTemplate.convertAndSend(exchangeName, "", message);
+    }
+}
